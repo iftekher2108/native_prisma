473d2c944820217ecaccf87ffd48052e6034fc36
iftekher2108 <parvezkhan21082002@gmail.com>
2025-12-29 22:05:03 +0600

    build fix

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>iftekher mahmud</Text>
-    </View>
+    <ScrollView className="p-2">
+      <View className="flex-1 justify-center items-center">
+        <Image className="w-20 h-20 rounded-full" source={require("../assets/image/iftekher.jpg")} />
+      <Text className="text-2xl mt-2 font-bold text-center">iftekher mahmud</Text>
+      </View>
+    </ScrollView>
   );
 }
